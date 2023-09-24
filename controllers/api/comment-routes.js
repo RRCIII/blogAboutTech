fca562ff28fa56ffd6ee56a81ddc68d527648091
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 12:34:44 -0400

    created router async await to get comments

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// GET COMMENTS
+router.get("/", async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findAll({
+      include: [{ model: Post, attributes: ["title", "content"] }],
+    });
+    if (!dbCommentData) {
+      res.status(404).json({ message: "No comments were found." });
+      return;
+    }
+    res.status(200).json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
