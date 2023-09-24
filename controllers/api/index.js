2ac5ab2ed5a044a64447f458295b88595e9e9041
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 14:15:29 -0400

    created get route to render login using sessions

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 const commentRoutes = require('./comment-routes');
 
 // All routes and controllers written for an open API; catering to different view implementations.
---
+++ dashboredRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      where: { user_id: req.session.user_id },
+    });
+    const data = dbPostData.map((post) => {
+      return post.get({ plain: true });
+    });
+
+    res.render("dashboard", { data, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
