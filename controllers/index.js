694d3f734db67f70156ca341a6e6c4600211429c
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 14:52:39 -0400

    updated routes per file naming convention

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes');
-const dashBoardRoutes = require('./dashboard-routes')
+const homeRoutes = require('./homeRoutes');
+const dashBoardRoutes = require('./dashboardRoutes')
 const apiRoutes = require('./api')
 
 router.use('/', homeRoutes);
