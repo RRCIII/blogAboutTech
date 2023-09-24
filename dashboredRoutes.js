2ed1ff29cd904de4dfb6eeca4cf4772fc8f46b7c
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 14:18:20 -0400

    created get route to handle individual post from user as well as export router

--- dashboredRoutes.js
+++ dashboredRoutes.js
@@ -20,3 +20,32 @@ router.get("/", async (req, res) => {
 router.get('/newpost', (req, res) => {
     res.render('dbnewpost', { loggedIn: req.session.loggedIn });
 })
+
+
+router.get("/:id", async (req, res) => {
+    try {
+      const dbPostData = await Post.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ["username"],
+          },
+          {
+            model: Comment,
+            include: {
+              model: User,
+              attributes: ["username"],
+            },
+          },
+        ],
+      });
+      const postData = dbPostData.get({ plain: true })
+  
+      res.render('edit-post', { postData, loggedIn: req.session.loggedIn })
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
