b3c56952f40a7913b3653b781d6194e54a6ce179
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 14:44:09 -0400

    created get route to handle posts via user and retur to homepage with given post

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    const blogPosts = dbPostData.map((blog) => {
+        return blog.get({ plain: true });
+      });
+      const isHomePage = req.url === '/'
+
+      res.render("homepage", {
+        blogPosts,
+        isHomePage,
+        loggedIn: req.session.loggedIn,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
