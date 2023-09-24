7f299711db59a3a09dac36e37b67ea29ce3c3ae7
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 15:36:19 -0400

    created user data

---
+++ seeds/userData.js
@@ -0,0 +1,33 @@
+const { User } = require("../models");
+
+const newUsers = [
+  {
+    username: "carolinaBoy_501",
+    email: "fifteenBackwoods@yahoo.com",
+    password: "qwer1234",
+  },
+  {
+    username: "ChanelBags<3",
+    email: "karenShmooves@gmail.com",
+    password: "qwer1234",
+  },
+  {
+    username: "bruce4Skin",
+    email: "uncutwarriors@gmail.com",
+    password: "qwer1234",
+  },
+  {
+    username: "DungSlinger69",
+    email: "cornboy23@icloud.com",
+    password: "qwer1234",
+  },
+  {
+    username: "conor_mcgregor",
+    email: "uLitlf00knWeeZl@hotmail.com",
+    password: "qwer1234",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(newUsers);
+
+module.exports = seedUsers;
