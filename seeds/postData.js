76c917e3bc22f9599c5a756c3fc7d36786033747
Ricky Carter <randolphcarter5@gmail.com>
2023-09-24 15:33:39 -0400

    created post data for seeds

---
+++ seeds/postData.js
@@ -0,0 +1,34 @@
+const { Post } = require('../models');
+
+const newPosts = [
+  {
+    title: 'Handlebars.js',
+    content: 'Templating engines are quite outdated now days.',
+    user_id: 2,
+  },
+  {
+    title: 'Fight Me.',
+    content: "yaLiTLweezel.",
+    user_id: 5,
+  },
+  {
+    title: 'Speaking of templating engines...',
+    content: 'curse be to this damn thing...dare challenge my indestructible wisdom.',
+    user_id: 4,
+  },
+  {
+    title: 'When designing an MVC pattern...',
+    content: 'Start with the model, followed by the controller, then the view. Ya dig?',
+    user_id: 3,
+  },
+  {
+    title: 'Coding bootcamps',
+    content:
+      "You'll be drowned in web development, in such a short amount of time. As difficult as it is, I can definitely say that I've learned a lot.",
+    user_id: 1,
+  },
+];
+
+const seedPosts = () => Post.bulkCreate(newPosts);
+
+module.exports = seedPosts;
