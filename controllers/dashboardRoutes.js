b2007ecf928c60bafa98edc5a93e365778af4ea8
Ricky Carter <randolphcarter5@gmail.com>
2023-10-02 13:12:37 -0400

    updated controllers dashboard file

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
-const withAuth = require("../utils");
+const withAuth = require("../utils/auth");
 
 // GET COMMENTS
 router.get("/", async (req, res) => {
