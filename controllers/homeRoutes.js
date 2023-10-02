44e8cd770c651432a9c81a3565f6a1de8a89326d
Ricky Carter <randolphcarter5@gmail.com>
2023-10-02 13:05:55 -0400

    updated homeRoutes file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,69 +20,69 @@ router.get("/", async (req, res) => {
       ],
     });
     const blogPosts = dbPostData.map((blog) => {
-        return blog.get({ plain: true });
-      });
-      const isHomePage = req.url === '/'
+      return blog.get({ plain: true });
+    });
+    const isHomePage = req.url === '/'
 
-      res.render("homepage", {
-        blogPosts,
-        isHomePage,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+    res.render("homepage", {
+      blogPosts,
+      isHomePage,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-  router.get("/post/:id", withAuth, async (req, res) => {
-    try {
-      const dbPostData = await Post.findByPk(req.params.id, {
-        include: [
-          {
+router.get("/post/:id", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          include: {
             model: User,
             attributes: ["username"],
           },
-          {
-            model: Comment,
-            include: {
-              model: User,
-              attributes: ["username"],
-            },
-          },
-        ],
-      });
-      const singlePost = dbPostData.get({ plain: true });
-      console.log(singlePost)
-      res.render("post", {
-        singlePost,
-        loggedIn: req.session.loggedIn,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
-  router.get('/newpost', (req, res) => {
-    res.render('hpnewpost', { loggedIn: req.session.loggedIn });
-  });
+        },
+      ],
+    });
+    const singlePost = dbPostData.get({ plain: true });
+    console.log(singlePost)
+    res.render("post", {
+      singlePost,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-  router.get("/login", (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
-    }
-    res.render("login");
-  });
+router.get('/newpost', (req, res) => {
+  res.render('hpnewpost', { loggedIn: req.session.loggedIn });
+});
 
-  router.get("/signup", (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
-    }
-    res.render("signup");
-  });
-  
-  module.exports = router;
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+module.exports = router;
   
