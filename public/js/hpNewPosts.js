c4b16fcb7bd7bce6fd2b68b84beb7d222eb2e3c7
Ricky Carter <randolphcarter5@gmail.com>
2023-10-02 13:03:09 -0400

    updated files to debug

--- public/js/hpNewPosts.js
+++ public/js/hpNewPosts.js
@@ -1,29 +1,29 @@
 const hpNewPost = async (event) => {
-    event.preventDefault();
-  
-    const title = document.getElementById("hp-post-title").value.trim();
-    const content = document.getElementById("hp-post-content").value.trim();
-  
-    if (title && content) {
-      try {
-        const response = await fetch("/api/posts", {
-          method: "POST",
-          body: JSON.stringify({ title, content }),
-          headers: { "Content-Type": "application/json" },
-        });
-  
-        if (response.ok) {
-          document.location.replace("/");
-        } else {
-          alert(`Post could not be created.`);
-        }
-      } catch (err) {
-        console.error("Error:", err);
-        alert(`An error occured while creating the post.`);
+  event.preventDefault();
+
+  const title = document.getElementById("hp-post-title").value.trim();
+  const content = document.getElementById("hp-post-content").value.trim();
+
+  if (title && content) {
+    try {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({ title, content }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace("/");
+      } else {
+        alert(`Post could not be created.`);
       }
-    } else {
-      alert("Please enter a title and content for the post.");
+    } catch (err) {
+      console.error("Error:", err);
+      alert(`An error occured while creating the post.`);
     }
-  };
-  
-  document.getElementById("hp-newpost-btn").addEventListener("click", hpNewPost);
+  } else {
+    alert("Please enter a title and content for the post.");
+  }
+};
+
+document.getElementById("hp-newpost-btn").addEventListener("click", hpNewPost);
