2c4e31c5cf4d9905cf476b594985e94e9cefa7e4
Ricky Carter <randolphcarter5@gmail.com>
2023-10-02 12:12:54 -0400

    updated naming convention

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
+const postRoutes = require('./postRoutes ');
 const commentRoutes = require('./comment-routes');
 
 // All routes and controllers written for an open API; catering to different view implementations.
