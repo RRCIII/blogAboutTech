7fef9ab4ad1a1504eb5bfa23be5a482f77fa918d
Ricky Carter <randolphcarter5@gmail.com>
2023-10-04 17:03:29 -0400

    updated dashboard file

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,90 +1,46 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../models");
-const withAuth = require("../utils/auth");
 
-// GET COMMENTS
 router.get("/", async (req, res) => {
   try {
-    const dbCommentData = await Comment.findAll({
-      include: [{ model: Post, attributes: ["title", "content"] }],
+    const dbPostData = await Post.findAll({
+      where: { user_id: req.session.user_id },
     });
-    if (!dbCommentData) {
-      res.status(404).json({ message: "No comments were found." });
-      return;
-    }
-    res.status(200).json(dbCommentData);
+    const data = dbPostData.map((post) => {
+      return post.get({ plain: true });
+    });
+
+    res.render("dashboard", { data, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-// GET SINGLE COMMENT
+router.get('/newpost', (req, res) => {
+    res.render('dbnewpost', { loggedIn: req.session.loggedIn });
+})
+
 router.get("/:id", async (req, res) => {
   try {
-    const dbCommentData = await Comment.findByPk(req.params.id, {
+    const dbPostData = await Post.findByPk(req.params.id, {
       include: [
         {
-          model: Post,
-          attributes: ["title", "content"],
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
         },
       ],
     });
-    if (!dbCommentData) {
-      res.status(404).json({ message: "No comment was found." });
-      return;
-    }
-    res.status(200).json(dbCommentData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+    const postData = dbPostData.get({ plain: true })
 
-// POST ~ CREATE NEW COMMENT
-router.post("/", withAuth, async (req, res) => {
-  try {
-    const { comment_text, post_id } = req.body;
-    const dbCommentData = await Comment.create({
-      comment_text,
-      post_id,
-      user_id: req.session.user_id,
-    });
-    res.status(201).json(dbCommentData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// PUT ~ UPDATE COMMENT
-router.put("/:id", withAuth, async (req, res) => {
-  try {
-    const dbCommentData = await Comment.update(req.body, {
-      where: { id: req.params.id },
-    });
-    if (dbCommentData[0] === 0) {
-      res.status(404).json({ message: "No comment was found to update." });
-      return;
-    }
-    res.status(200).json(dbCommentData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// DELETE ~ DELETE COMMENT
-router.delete("/:id", withAuth, async (req, res) => {
-  try {
-    const dbCommentData = await Comment.destroy({
-      where: { id: req.params.id },
-    });
-    if (!dbCommentData) {
-      res.status(404).json({ message: "No comment was found to delete." });
-      return;
-    }
-    res.status(204).json(dbCommentData);
+    res.render('edit-post', { postData, loggedIn: req.session.loggedIn })
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
